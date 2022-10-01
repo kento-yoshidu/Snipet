03b6d82f8688d3e886b255a31449f84a941a2532
kento <kento@toriwatari.bird>
2022-10-01 14:30:58 +0900

    #8 型定義ファイルを作成

---
+++ src/@types/types.d.ts
@@ -0,0 +1,10 @@
+export interface Frontmatter {
+  title: string
+  seriesSlug: string
+  seriesName: string
+  postdate: string
+  update: string
+  series: string
+  tags: string[]
+  description: string
+}
