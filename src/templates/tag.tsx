c1fb69c7bda757f3aaa00e4b99b40cf6c9040069
kento <kento@toriwatari.bard>
2023-01-01 14:51:16 +0900

    ✨ Update

--- src/templates/tag.tsx
+++ src/templates/tag.tsx
@@ -3,12 +3,31 @@ import { graphql, PageProps } from "gatsby"
 
 import Layout from "../components/layout"
 import PostList from "../components/postList"
+import PageTitle from "../components/pageTitle"
+import Seo from "../components/seo"
 
-const Tag = ({ data }: PageProps<Queries.TagQuery>) => {
+type PageContext = {
+  postCount: number,
+  pageCount: number,
+  skip: number,
+  limit: number,
+  currentPage: number
+  isFirst: boolean
+  isLast: boolean
+  tag: string
+}
+
+const Tag = ({ data, pageContext }: { data: Queries.TagQuery, pageContext: PageContext }) => {
+  const { tag } = pageContext
   const { nodes } = data.allMarkdownRemark
 
   return (
     <Layout>
+      <PageTitle
+        title={`${tag} タグの記事一覧`}
+        count={nodes.length}
+      />
+
       {nodes.map((node) => (
         <PostList
           slug={node!.fields!.slug!}
@@ -24,6 +43,14 @@ const Tag = ({ data }: PageProps<Queries.TagQuery>) => {
 }
 
 export default Tag
+
+export const Head = ({ pageContext }: { pageContext: PageContext }) => {
+  return (
+    <Seo
+      title={`${pageContext.tag} タグの記事一覧`}
+    />
+  )
+}
 
 export const pageQuery = graphql`
   query Tag(
